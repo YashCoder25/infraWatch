6051d1756a2daa8a51de0fdac7e5147d71e8156d
= <yashgohel55318@gmail.com>
2026-08-22 17:24:57 +0530

    add project

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // Set base to the GitHub Pages repo sub-path so asset URLs are correct.
   base: '/infraWatch/',
   plugins: [react()],
   server: {
